46dd13fed91a919907a23d4ede78a541a6d2ed56
Terrance Rose Jr <trose618@gmail.com>
2018-10-12 20:25:38 +0000

    automatically backed up by learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -15,13 +15,13 @@ def greeting(greeting, name)
 end
 
 def return_a_value
-"Nice"
+  "Nice"
 end
 
 def last_evaluated_value
-"expert"
+  "expert"
 end
 
 def pizza_party(topping = "cheese")
-topping
+  topping
 end
